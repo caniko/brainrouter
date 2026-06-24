46c99a20442c40a8fd3b91ae428a981fc8d8a80e
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-06-24 23:25:50 +0200

    flake: fix rs-harbor url (codeberg, not github)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rs-harbor = {
-      url = "github:caniko/rs-harbor";
+      url = "git+https://codeberg.org/caniko/rs-harbor.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     rust-overlay = {
